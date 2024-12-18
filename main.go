b23676b77f0a9ecab3276fdf8c45c3c0145a55f5
w0lfraz0r <wolfrazorwithout0@gmail.com>
2024-12-18 21:18:01 +0530

    Implemented -c

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "flag"
+    "io/ioutil"
+    "os"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+    // Define command-line flags
+    countBytes := flag.Bool("c", false, "Count bytes in the file")
+
+    flag.Parse()
+
+    // Check if a file is provided
+    if len(flag.Args()) == 0 {
+        fmt.Println("Usage: ccwc [OPTIONS] [FILE]")
+        os.Exit(1)
+    }
+
+    // Read the file
+    filename := flag.Args()[0]
+    content, err := ioutil.ReadFile(filename)
+    if err != nil {
+        fmt.Printf("Error reading file: %v\n", err)
+        os.Exit(1)
+    }
+
+    // Handle -c flag
+    if *countBytes {
+        fmt.Printf("%8d %s\n", len(content), filename)
+    }
 }
