65536396fbd92c255f8ab68c15eae6762776a232
w0lfraz0r <wolfrazorwithout0@gmail.com>
2024-12-19 09:36:59 +0530

    implemented -l

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
     "flag"
     "io/ioutil"
     "os"
+    "bufio"
 )
 
 func main() {
     // Define command-line flags
     countBytes := flag.Bool("c", false, "Count bytes in the file")
+    countLines := flag.Bool("l", false, "Count newlines in the file")
 
     flag.Parse()
 
@@ -30,5 +32,28 @@ func main() {
     // Handle -c flag
     if *countBytes {
         fmt.Printf("%8d %s\n", len(content), filename)
+    }
+
+    // Handle -l flag
+    if *countLines {
+        file, err := os.Open(filename)
+        if err != nil {
+            fmt.Printf("Error opening file: %v\n", err)
+            os.Exit(1)
+        }
+        defer file.Close()
+
+        scanner := bufio.NewScanner(file)
+        lineCount := 0
+        for scanner.Scan() {
+            lineCount++
+        }
+
+        if err := scanner.Err(); err != nil {
+            fmt.Printf("Error reading file: &v\n", err)
+            os.Exit(1)
+        }
+
+        fmt.Printf("%8d %s\n", lineCount, filename)
     }
 }
